d79f5c31eb5576df078cda879cbee777e2840875
titkonen <toni.itkonen@icloud.com>
2020-02-10 22:25:04 +0200

    Uploading files

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import Button from './components/Button';
+
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0
+    }
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
+  render() {
+    let { count } = this.state
+    return (
+      <div className="App">
+        <h2>Count: { count }</h2>
+        <Button 
+          title = { "+" }
+          task = { () => this.incrementCount() }
+        />
+        <Button
+          title = { "-" }
+          task = { () => this.decrementCount() }
+        />
+
+
+
+<h2>Count2: { count }</h2>
+<Button 
+          title = { "+" }
+          task = { () => this.incrementCount() }
+        />
+        <Button
+          title = { "-" }
+          task = { () => this.decrementCount() }
+        />
+
+
+      </div>
+
+
+
+
+
+
+
+
+    );
+  }
+}
